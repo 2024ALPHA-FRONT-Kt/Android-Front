36505319464bc7adb0e6f7f78bd193556943c4dc
syngrxm <smcubez48@naver.com>
2024-05-09 17:44:51 +0900

    [UI] disc initial & test

---
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/disc/DiscStartViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.myapplication.ui.disc
+
+import androidx.lifecycle.ViewModel
+
+class DiscStartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/disc/DiscTestFragment.kt
@@ -0,0 +1,31 @@
+package com.android.myapplication.ui.disc
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.myapplication.R
+
+class DiscTestFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DiscTestFragment()
+    }
+
+    private val viewModel: DiscTestViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_disc_test, container, false)
+    }
+}
