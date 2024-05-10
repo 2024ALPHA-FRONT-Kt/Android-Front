52470de3cb544ff6fb0604fdb70c3848746712d5
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-05-10 16:26:07 +0900

    [UI] SignIn IMG

--- ALPHA/app/src/main/java/com/android/myapplication/ui/mypage/MyPageFragment.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/mypage/MyPageFragment.kt
@@ -1,4 +1,4 @@
-package com.android.myapplication.ui.dashboard
+package com.android.myapplication.ui.mypage
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.android.myapplication.databinding.FragmentDashboardBinding
+import com.android.myapplication.databinding.FragmentMypageBinding
 
-class DashboardFragment : Fragment() {
+class MyPageFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentMypageBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(MyPageViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
--- ALPHA/app/src/main/java/com/android/myapplication/ui/mypage/MyPageViewModel.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/mypage/MyPageViewModel.kt
@@ -1,10 +1,10 @@
-package com.android.myapplication.ui.dashboard
+package com.android.myapplication.ui.mypage
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class MyPageViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
