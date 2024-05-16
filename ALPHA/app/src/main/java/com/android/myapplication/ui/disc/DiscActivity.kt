40745bfde222f503f966438c0efdc6a317bef178
syngrxm <smcubez48@naver.com>
2024-05-16 13:35:19 +0900

    [FIX] file structure

--- ALPHA/app/src/main/java/com/android/myapplication/ui/disc/DiscActivity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/disc/DiscActivity.kt
@@ -12,7 +12,7 @@ class DiscActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_disc)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.disc_start)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
