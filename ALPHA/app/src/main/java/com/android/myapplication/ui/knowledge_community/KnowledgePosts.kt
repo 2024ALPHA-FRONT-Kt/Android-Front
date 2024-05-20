b9ca19f6de0ec7377df1bf9f95852107c15281d8
sumin Shin <110521260+syngrxm@users.noreply.github.com>
2024-05-20 22:40:25 +0900

    Revert "[MERGE] Knowledge Plus Community UI"

--- ALPHA/app/src/main/java/com/android/myapplication/ui/knowledge_community/KnowledgePosts.kt
+++
@@ -1,7 +0,0 @@
-package com.android.myapplication.ui.knowledge_community
-
-data class KnowledgePosts(
-    var view_knowledge_posts_title : String,
-    var view_knowledge_posts_content : String,
-    var view_knowledge_posts_date : String
-)
