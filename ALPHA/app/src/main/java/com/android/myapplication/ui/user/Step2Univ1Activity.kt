cfa45ac2e9e8ab9ee40024a5667250130980a1e5
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-05-24 01:51:02 +0900

    [FEATURE] step2univ1 로직

--- ALPHA/app/src/main/java/com/android/myapplication/ui/user/Step2Univ1Activity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/user/Step2Univ1Activity.kt
@@ -1,10 +1,13 @@
 package com.android.myapplication.ui.user
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.myapplication.MainActivity
 import com.android.myapplication.R
 import com.android.myapplication.databinding.ActivityEditBinding
 import com.android.myapplication.databinding.ActivityStep2Univ1Binding
@@ -15,9 +18,41 @@ class Step2Univ1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step2_univ1)
         supportActionBar?.hide()
+        var newUniv = ""
+        var newEmail = ""
 
         // 바인딩
         binding = ActivityStep2Univ1Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnGetPasskey.setOnClickListener {
+            newUniv = binding.newUnivU.toString()
+            newEmail = binding.newEmail.toString()
+        }
+
+        var valid = 0 // 인증결과
+        binding.btnCheckPasskey.setOnClickListener {
+            // 인증잘되면
+            valid = 1
+            binding.newUnivU.isEnabled = false
+            binding.univEmail.isEnabled = false
+            Toast.makeText(applicationContext,"인증이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            // 안되면
+            Toast.makeText(applicationContext,"이메일 인증을 다시 해주세요", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnNext.setOnClickListener {
+            if (valid == 1){
+                val intent = Intent(this, Step2Univ2Activity::class.java)
+                intent.putExtra("univ",newUniv)
+                intent.putExtra("email",newEmail)
+                startActivity(intent)
+            } else {
+                Toast.makeText(applicationContext,"이메일 인증이 완료되지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
     }
 }
