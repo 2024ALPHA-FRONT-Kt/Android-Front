1786d8766bfc6bd6d5d8cc2b54dcb19c4e5fd0c7
syngrxm <smcubez48@naver.com>
2024-05-23 17:25:55 +0900

    [FEATURE] load the number of users participating in the test

--- ALPHA/app/src/main/java/com/android/myapplication/ui/disc/DiscActivity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/disc/DiscActivity.kt
@@ -2,25 +2,48 @@ package com.android.myapplication.ui.disc
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.android.myapplication.R
+import com.android.myapplication.App
+import com.android.myapplication.MainActivity
+import com.android.myapplication.api.RetrofitClient
+import com.android.myapplication.databinding.ActivityDiscBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DiscActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDiscBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_disc)
         supportActionBar?.hide()
 
-        findViewById<Button>(R.id.disc_test_start_button).setOnClickListener {
-            val intent = Intent(this, DiscTestActivity::class.java)
-            startActivity(intent)
+        val apiService = RetrofitClient.apiservice
+        binding = ActivityDiscBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val globalAccessToken: String = App.prefs.getItem("accessToken", "no Token")
+        val token = "Bearer ${globalAccessToken.replace("\"", "")}"
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val responseData = apiService.getDiscUsers(token)
+                val discUsers = responseData.data // 이미 int라서 바꿀 수가 없는 것 같은데 우째야 합미까
+                withContext(Dispatchers.Main) {
+                    binding.discUsers.text = discUsers.toString()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
 
-        findViewById<ImageView>(R.id.disc_start_page_back).setOnClickListener {
-            val intent = // todo
-            startActivity(intent)
+        binding.discTestStartButton.setOnClickListener {
+            startActivity(Intent(this, DiscTestActivity::class.java))
+        }
+
+        binding.discStartPageBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
