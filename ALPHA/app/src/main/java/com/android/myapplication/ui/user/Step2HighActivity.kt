c90b93fdb401918d2260413173d18da1d7e7a39d
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-05-23 22:41:45 +0900

    [FEATURE] step2 1차

--- ALPHA/app/src/main/java/com/android/myapplication/ui/user/Step2HighActivity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/user/Step2HighActivity.kt
@@ -6,11 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.android.myapplication.R
+import com.android.myapplication.databinding.ActivityStep2HighBinding
 
 class Step2HighActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStep2HighBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step2_high)
         supportActionBar?.hide()
+
+        val newName = binding.newName
+        val newAge = binding.newAge
+        val newMan = binding.newMan
+        val newWoman = binding.newWoman
+        val newId = binding.newId
+        val newPw = binding.newPw
+        val newPwRe = binding.newPwRe
+        val newUnivH = binding.newUnivH
+        val newDepartH = binding.newDepartH
+
+        // 성별 확인
+        // 아이디 중복 확인
+        // 비밀번호 일치 확인
     }
 }
--- ALPHA/app/src/main/java/com/android/myapplication/ui/user/Step2Univ1Activity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/user/Step2Univ1Activity.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.android.myapplication.R
+import com.android.myapplication.databinding.ActivityStep2Univ1Binding
 
 class Step2Univ1Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityStep2Univ1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step2_univ1)
