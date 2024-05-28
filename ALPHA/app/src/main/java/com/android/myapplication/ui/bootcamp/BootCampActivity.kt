e43c4fe9f082c6d88a61d604eeb27aeccce22233
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-05-28 14:15:04 +0900

    [UI] bootcamp activity 생성

--- ALPHA/app/src/main/java/com/android/myapplication/ui/bootcamp/BootCampActivity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/bootcamp/BootCampActivity.kt
@@ -1,9 +1,11 @@
 package com.android.myapplication.ui.bootcamp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.android.myapplication.R
 import com.android.myapplication.databinding.ActivityBootCampBinding
+import com.android.myapplication.ui.disc.DiscTestActivity
 
 class BootCampActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBootCampBinding
@@ -15,5 +17,14 @@ class BootCampActivity : AppCompatActivity() {
         // 바인딩
         binding = ActivityBootCampBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bootCampStartBack.setOnClickListener {
+            onBackPressed()
+        }
+
+        binding.bootCampStartButton.setOnClickListener{
+            val intent = Intent(this, SelectCampActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
