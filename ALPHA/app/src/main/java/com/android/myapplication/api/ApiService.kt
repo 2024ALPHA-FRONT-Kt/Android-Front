4b808326478f65736d110309de0876f86839f34c
syngrxm <smcubez48@naver.com>
2024-05-28 10:04:02 +0900

    [FEATURE] load free posts

--- ALPHA/app/src/main/java/com/android/myapplication/api/ApiService.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/api/ApiService.kt
@@ -2,12 +2,15 @@ package com.android.myapplication.api
 
 import com.android.myapplication.dto.EditProfile
 import com.android.myapplication.dto.ResponseObject
+import com.android.myapplication.ui.free_community.data_class.EditingFree
+import com.android.myapplication.ui.free_community.data_class.PostingFree
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.PUT
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 // API 인터페이스 정의
@@ -21,4 +24,40 @@ interface ApiService {
     @PUT("/user")
     suspend fun editProfile(@Header("Authorization") Authorization: String,@Body data: EditProfile): ResponseObject
 
+    @GET("/posts")
+    suspend fun freeLists(
+        @Header("Authorization") authorization: String,
+        @Query("postType") postType: String,
+        @Query("page") page: Int
+    ) : ResponseObject
+
+    @POST("/post")
+    suspend fun postingKnowledgePost(
+        @Header("Authorization") authorization: String,
+        @Body freePost: PostingFree
+    ) : ResponseObject
+
+    @DELETE("/post")
+    suspend fun deleteFree(
+        @Header("Authorization") authorization: String,
+        @Query("id") id: String
+    ) : ResponseObject
+
+    @PATCH("/post")
+    suspend fun editFree(
+        @Header("Authorization") authorization: String,
+        @Body editFree: EditingFree
+    ) : ResponseObject
+
+    @GET("/post")
+    suspend fun freePostDetail(
+        @Header("Authorization") authorization: String,
+        @Query("id") id: String
+    ) : ResponseObject
+
+    @GET("/hot-post")
+    suspend fun loadHotFreePost(
+        @Header("Authorization") authorization: String,
+        @Query("postType") postType: String
+    ) : ResponseObject
 }
--- ALPHA/app/src/main/java/com/android/myapplication/ui/free_community/FreePostListFragment.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/free_community/FreePostListFragment.kt
@@ -6,12 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.myapplication.App
+import com.android.myapplication.api.RetrofitClient
 import com.android.myapplication.databinding.FragmentFreePostListBinding
+import com.android.myapplication.ui.knowledge_community.KnowledgePosts
+import com.android.myapplication.ui.knowledge_community.KnowledgePostsAdapter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class FreePostListFragment : Fragment() {
 
     private var _binding: FragmentFreePostListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: FreePostAdapter
+    private var currentPage = 0
+
+    private val apiService = RetrofitClient.apiservice
+    private val gson = Gson()
+    private val globalAccessToken: String = App.prefs.getItem("accessToken", "no Token")
+    private val token = "Bearer ${globalAccessToken.replace("\"", "")}"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,14 +41,40 @@ class FreePostListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val list = ArrayList<FreePosts>()
-        list.add(FreePosts("제목 1", "내용 1"))
-        list.add(FreePosts("제목 2", "내용 2"))
-        list.add(FreePosts("제목 3", "내용 3"))
-
         binding.freeListView.layoutManager = LinearLayoutManager(context)
-        val adapter = FreePostAdapter(list)
         binding.freeListView.adapter = adapter
+
+        binding.freeListView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                if (layoutManager.findLastCompletelyVisibleItemPosition() == adapter.itemCount - 1) {
+                    currentPage++
+                    loadPosts(currentPage)
+                }
+            }
+        })
+        loadPosts(currentPage)
+    }
+
+    private fun loadPosts(page: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = apiService.freeLists(
+                    token,
+                    "FREE",
+                    page
+                )
+                val dataJson = gson.toJson(response.data)
+                val type = object : TypeToken<List<FreePosts>>() {}.type
+                val newPosts: List<FreePosts> = gson.fromJson(dataJson, type)
+                CoroutineScope(Dispatchers.Main).launch {
+                    adapter.addPosts(newPosts)
+                }
+            } catch (e: Exception) {
+                // todo
+            }
+        }
     }
 
     override fun onDestroyView() {
