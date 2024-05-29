a5156d7e2f0d49be4501b6aebeeebc1f83261a65
synrxm <110521260+syngrxm@users.noreply.github.com>
2024-05-29 21:46:07 +0900

    [FEATURE] add edit page

--- ALPHA/app/src/main/java/com/android/myapplication/ui/knowledge_community/KnowledgePostsAdapter.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/knowledge_community/KnowledgePostsAdapter.kt
@@ -1,21 +1,35 @@
 package com.android.myapplication.ui.knowledge_community
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.android.myapplication.App
 import com.android.myapplication.R
+import com.android.myapplication.api.RetrofitClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class KnowledgePostsAdapter(private val items: ArrayList<KnowledgePosts>, private val onItemClick: (KnowledgePosts) -> Unit) : RecyclerView.Adapter<KnowledgePostsAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int = items.size
-
+    val apiService = RetrofitClient.apiservice
+    val globalAccessToken: String = App.prefs.getItem("accessToken", "no Token")
+    val token = "Bearer ${globalAccessToken.replace("\"", "")}"
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.bind(item)
         holder.itemView.setOnClickListener { onItemClick(item) }
+        GlobalScope.launch(Dispatchers.IO) {
+            val page = 0
+            val postType = "QNA" // 고정값
+            val responseData = apiService.knowLedgeLists(token, postType, page)
+            Log.d("dmd!!", responseData.toString())
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
--- ALPHA/app/src/main/java/com/android/myapplication/ui/knowledge_community/ViewKnowledgePostActivity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/knowledge_community/ViewKnowledgePostActivity.kt
@@ -1,9 +1,14 @@
 package com.android.myapplication.ui.knowledge_community
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import com.android.myapplication.App
+import com.android.myapplication.R
 import com.android.myapplication.api.RetrofitClient
 import com.android.myapplication.databinding.ActivityViewKnowledgePostBinding
 import com.android.myapplication.ui.knowledge_community.data_class.ViewingKnowledge
@@ -30,6 +35,9 @@ class ViewKnowledgePostActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         fetchKnowledgePostDetail()
+        binding.viewKnowledgePostMenu.setOnClickListener {
+            popup(binding.viewKnowledgePostMenu)
+        }
     }
 
     private fun fetchKnowledgePostDetail() {
@@ -50,6 +58,43 @@ class ViewKnowledgePostActivity : AppCompatActivity() {
             } catch (e: Exception) {
                 Log.e("ViewKnowledgePostActivity", e.toString())
             }
+        }
+    }
+
+    private fun popup(v: View) {
+        val popup = PopupMenu(this, v)
+        popup.menuInflater.inflate(R.menu.k_plus_q_menu, popup.menu)
+        popup.setOnMenuItemClickListener { item ->
+            clickMenu(item)
+        }
+        popup.show()
+    }
+
+    private fun clickMenu(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.k_plus_q_menu_3 -> {
+                GlobalScope.launch(Dispatchers.IO) {
+                    try {
+                        val responseData = apiService.knowledgePostDetail(token, "556ba748-d8b4-4258-8333-4497697a1a67")
+                        val jsonObject = gson.fromJson(responseData.data.toString(), JsonObject::class.java)
+                        val data = gson.fromJson(jsonObject, ViewingKnowledge::class.java)
+                        val editTitleDraft = data.title
+                        val editContentDraft = data.content
+
+                        withContext(Dispatchers.Main) {
+                            val intent = Intent(this@ViewKnowledgePostActivity, WriteKnowledgePostActivity::class.java).apply {
+                                putExtra("editTitleDraft", editTitleDraft)
+                                putExtra("editContentDraft", editContentDraft)
+                            }
+                            startActivity(intent)
+                        }
+                    } catch (e: Exception) {
+                        Log.e("ViewKnowledgePostActivity", e.toString())
+                    }
+                }
+                true
+            }
+            else -> false
         }
     }
 }
