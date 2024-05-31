3c9db68d28ba673b11c7fd8c8f4aa9dfce3c7d83
synrxm <110521260+syngrxm@users.noreply.github.com>
2024-06-01 00:36:23 +0900

    [FEATURE] don't move page if comment not exists

--- ALPHA/app/src/main/java/com/android/myapplication/ui/free_community/ViewFreePostActivity.kt
+++ ALPHA/app/src/main/java/com/android/myapplication/ui/free_community/ViewFreePostActivity.kt
@@ -3,6 +3,7 @@ package com.android.myapplication.ui.free_community
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.myapplication.App
 import com.android.myapplication.api.RetrofitClient
@@ -10,6 +11,7 @@ import com.android.myapplication.databinding.ActivityViewFreePostBinding
 import com.android.myapplication.ui.free_community.data_class.PostingComment
 import com.android.myapplication.ui.free_community.data_class.ViewingFree
 import com.android.myapplication.ui.knowledge_community.ViewFreePostPlusActivity
+import com.android.myapplication.ui.knowledge_community.ViewKnowledgePostActivity
 import com.android.myapplication.ui.knowledge_community.ViewKnowledgePostWithAnswerActivity
 import com.google.gson.Gson
 import com.google.gson.JsonObject
@@ -17,6 +19,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.system.exitProcess
 
 class ViewFreePostActivity : AppCompatActivity() {
 
@@ -26,6 +29,7 @@ class ViewFreePostActivity : AppCompatActivity() {
     private val globalAccessToken: String = App.prefs.getItem("accessToken", "no Token")
     private val token = "Bearer ${globalAccessToken.replace("\"", "")}"
     private lateinit var postId: String
+    private var existResponseComment: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +53,7 @@ class ViewFreePostActivity : AppCompatActivity() {
             val jsonObject = gson.fromJson(responseData.data.toString(), JsonObject::class.java)
             val data = gson.fromJson(jsonObject, ViewingFree::class.java)
             val uId = data.email.split("@")[0]
+            existResponseComment = data.responseCommentDto.size > 0
 
             withContext(Dispatchers.Main) {
                 binding.viewFreePostTitle.text = data.title
@@ -62,11 +67,17 @@ class ViewFreePostActivity : AppCompatActivity() {
         }
 
         binding.freePostCommentPlusButton.setOnClickListener {
-            val intent = Intent(this, ViewFreePostPlusActivity::class.java)
-            intent.putExtra("fromViewPostId", postId)
-            startActivity(intent)
-            finish()
+            if (existResponseComment) {
+                val intent = Intent(this, ViewFreePostPlusActivity::class.java)
+                intent.putExtra("fromViewPostId", postId)
+                intent.putExtra("fromViewPlustButton", true)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "불러올 수 있는 댓글이 없습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
+
 
         binding.viewFreePostBackButton.setOnClickListener {
             val intent = Intent(this, FreePostListActivity::class.java)
